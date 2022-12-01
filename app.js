bee6ebb1096f43ea3acf882193ca466258b763d6
Karan Pun <karanpun011@gmail.com>
2022-11-30 20:37:17 -0500

    add css and finish the project

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const port = 3000;
-
-let newTodoLists = ['do workout'];
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}));
+//use the ejs
+app.set('view engine', 'ejs');
+//bodyparser extension
+app.use(bodyParser.urlencoded({extended:true}));
+//treat public dir as a static files container
+app.use(express.static('public'));
 
-app.get("/", function(req, res){
+//make a list of todos
+let newTodoItems = ['write journal', 'gym workout', 'work'];
 
-    let todayDate = new Date();
+app.get('/', function(req, res) {
+    var today = new Date();
 
-    res.render('list', {
-        todoLists: newTodoLists 
-    })
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    }
 
+    //change the date format in string
+    let day = today.toLocaleDateString('en-US', options)
+
+    //send the updated date to the template file 
+    res.render("list", {
+        kindOfDay: day,
+        todoLists: newTodoItems
+    });
 });
 
 app.post('/', function(req, res) {
-    newTodoLists.push(req.body.newTodo);
+    //get data from html form and add to array
+    newTodoItems.push(req.body.todoItem);
+    //redirecting the user to root
     res.redirect('/');
-});
-
-app.listen(port, function(){
-    console.log('hit hit success on port 3000');
 })
+app.listen(port, function() {
+    console.log('hit hit -- 3000')
+});
